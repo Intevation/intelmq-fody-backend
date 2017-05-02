628f1cfd6fd4769e73cce88aaf8e2b0069e18e94
Dustin Demuth <dustin@intevation.de>
2017-05-02 14:49:56 +0200

    Renamed the API to Backend

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name='intelmq-fody-api',
+    name='intelmq-fody-backend',
     version='0.4.3.dev0',
     packages=['tickets_api.tickets_api', 'events_api.events_api', 'contactdb_api.contactdb_api',
               'checkticket_api.checkticket_api', 'intelmq_fody_api'],
