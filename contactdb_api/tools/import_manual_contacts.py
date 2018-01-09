06c90ff7262cba89ad50583e80fd77cc5a2ad4c9
Bernhard Reiter <bernhard@intevation.de>
2018-01-09 15:02:38 +0100

    tools: improves import_manual_contacts.py
     * Completes the building of the import data structure. It can be
       already used to dump and manually import the data as documented.
     * Adds another bad input example that is missing a `contact` value.

--- contactdb_api/tools/import_manual_contacts.py
+++ contactdb_api/tools/import_manual_contacts.py
@@ -4,11 +4,23 @@
 Takes contacts from a special .csv file and a parameter for a tag.
 Just imports, does **not** check if the data is in the contactdb already.
 
-Example input:
+Example:
     {scriptname} example-contacts-1.csv "Targetgroup:CRITIS"
 
-A tool to be used as part of an intelmq-cb-mailgen
-(https://github.com/Intevation/intelmq-mailgen-release) setup.
+Details:
+    All rows with the same value in `organization` will be added to the same
+    organisation. It is assumed that the `contact` value is the same for all
+    those rows: a list of plain email addresses, see example-contacts-1.csv.
+
+    An "import_YYYYMMDD" comment is added to the organisation.
+
+    The result of --dry-run --dump-json can be used to manually upload, e.g.
+        curl http://localhost:8070/api/contactdb/org/manual/commit \
+                --header "Content-Type:application/json" --data @z
+
+Context:
+    A tool to be used as part of an intelmq-cb-mailgen
+    (https://github.com/Intevation/intelmq-mailgen-release) setup.
 
 
 Copyright (C) 2017,2018 by Bundesamt für Sicherheit in der Informationstechnik
@@ -36,8 +48,9 @@ Author(s):
 import argparse
 import csv
 import datetime
-# from email.utils import parseaddr
+from email.utils import getaddresses
 import ipaddress
+import json
 import logging
 import pprint
 import sys
@@ -45,29 +58,46 @@ import sys
 log = logging.getLogger(__name__)
 logging.basicConfig(format='%(levelname)s:%(message)s',
                     level=logging.INFO)
-#                    level=logging.DEBUG)
 
 import_comment = "import_" + datetime.date.today().strftime("%Y%m%d")
 
 
 def add_info_from_row(orgs_by_name, line_number, row, tag):
     """Add info from one row to the orgs_by_name dictionary."""
+
+    # email.utils.getaddresses calls parseaddr, but does not do much checking.
+    email_addresses = getaddresses([row["contact"]])
+    if len(email_addresses) < 1:
+        log.error("No email addresses found in line %d", line_number)
+        raise ValueError("is no list email addressses", row["contact"])
+
+    contacts = []
+    for realname, email_addr in email_addresses:
+        # /!\ we assume no realnames
+        contacts.append({
+            "comment": "",
+            "email": email_addr,
+            "firstname": "",
+            "lastname": "",
+            "openpgp_fpr": "",
+            "tel": "",
+            })
+
     # find or add organization
     new_org = orgs_by_name.setdefault(row["organization"], {
-        "name": row["organization"],
-        "comment": import_comment,
-        "ripe_org_hdl": "",
-        "ti_handle": "",
-        "first_handle": "",
-        "sector_id": None,
-        "networks": [],
-        "asns": [],
-        "fqdns": [],
         "annotations": [{"tag": tag}],
+        "asns": [],
+        "comment": import_comment,
+        "contacts": contacts,
+        "first_handle": "",
+        "fqdns": [],
+        "name": row["organization"],
         "national_certs": [],
+        "networks": [],
+        "ripe_org_hdl": "",
+        "sector_id": None,
+        "ti_handle": "",
         })
-
-    # TODO add contact(s)
 
     if row["as_or_cidr"].startswith("AS"):
         try:
@@ -92,6 +122,7 @@ def add_info_from_row(orgs_by_name, line_number, row, tag):
             raise
 
         new_org["networks"].append({"address": cidr,
+                                    "annotations": [],
                                     "comment": row["comment"]})
 
 
@@ -103,6 +134,8 @@ def main():
                         help="set loglevel to DEBUG")
     parser.add_argument("--dry-run", action="store_true",
                         help="just print resulting org-objects")
+    parser.add_argument("--dump-json", action="store_true",
+                        help="output json commands when dry-running")
     parser.add_argument("filename", help="file to import")
     parser.add_argument("tag", help="tag to be used for importing")
 
@@ -124,8 +157,16 @@ def main():
             log.debug(row)
             add_info_from_row(orgs_by_name, line_number, row, args.tag)
 
+    orgs = list(orgs_by_name.values())
+    fody_backend_commands = ["create"] * len(orgs)
+
     if args.dry_run:
-        pprint.pprint(orgs_by_name)
+        if args.dump_json:
+            print(json.dumps({"commands": fody_backend_commands,
+                              "orgs": orgs}, sort_keys=True, indent=4))
+        else:
+            pprint.pprint(orgs_by_name)
+
     else:
         # TODO real import
         raise NotImplementedError
