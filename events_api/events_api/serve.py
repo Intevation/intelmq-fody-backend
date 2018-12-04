fbc8df35f3d17f370f237a30c1f265b6bcf65e74
Sebastian Wagner <wagner@cert.at>
2018-12-04 11:13:17 +0100

    stats: reconnect to postgres in case of error
    reconnects if no cursor can be fetched
    still leads to one 500 connection interestingly, but the next will
    succeed

--- events_api/events_api/serve.py
+++ events_api/events_api/serve.py
@@ -464,7 +464,12 @@ def query(prepared_query):
 
     # psycopgy2.4 does not offer 'with' for cursor()
     # FUTURE use with
-    cur = eventdb_conn.cursor(cursor_factory=RealDictCursor)
+    try:
+        cur = eventdb_conn.cursor(cursor_factory=RealDictCursor)
+    except (psycopg2.InterfaceError, psycopg2.InternalError,
+            psycopg2.OperationalError, AttributeError):
+        setup(None)
+        cur = eventdb_conn.cursor(cursor_factory=RealDictCursor)
 
     operation = prepared_query[0]
     parameters = prepared_query[1]
