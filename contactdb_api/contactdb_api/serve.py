b5d06448283b1b853fe7156ebc90663735f338ba
Paul Höller <199104660+paul-hoeller@users.noreply.github.com>
2025-04-22 00:52:24 +0200

    Allow capital letters

--- contactdb_api/contactdb_api/serve.py
+++ contactdb_api/contactdb_api/serve.py
@@ -862,7 +862,7 @@ create_org_schema = {
                 "properties": {
                     "firstname": {"type": "string"},
                     "lastname": {"type": "string"},
-                    "email": {"type": "string", "pattern": "^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$"},
+                    "email": {"type": "string", "pattern": "^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$"},
                     "tel": {"type": "string"},
                     "comment": {"type": "string"},
                     "openpgp_fpr": {"type": "string"}
@@ -875,7 +875,7 @@ create_org_schema = {
             "items": {
                 "type": "object",
                 "properties": {
-                    "fqdn": {"type": "string", "pattern": "^(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$"},
+                    "fqdn": {"type": "string", "pattern": "^(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$"},
                     "comment": {"type": "string"},
                     "annotations": {"$ref": "#/$defs/annotations"}
                 },
